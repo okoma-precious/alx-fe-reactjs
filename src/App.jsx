6fc9198ddd11ae201877ec78fdc4bec2ae43d900
Okoma Precious <okomaprecious16@gmail.com>
2025-07-16 23:34:53 +0100

    Add UserProfile

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Precious" age="25" bio="Loves coding and football." />
       <Footer />
     </>
   );
