097065caba0e69131173e0e3438d74db796f86fe
Okoma Precious <okomaprecious16@gmail.com>
2025-07-16 23:29:18 +0100

    Add Header, MainContent and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-// src/App.jsx
-
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <WelcomeMessage />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+}
+
+export default Header;
+// This code defines a simple React component called Header that renders a header element with a title "My Favorite Cities".
